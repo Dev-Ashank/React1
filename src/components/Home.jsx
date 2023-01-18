e5d77dd340e17ef17de4213dc2b72e7e7a5b6423
KUMAR DIVYASHANK GAURAV <kumardivyashankgaurav@gmail.com>
2023-01-19 05:17:00 +0530

    Impl. Header, Home,and its Css

---
+++ src/components/Home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "../styles/Home.scss";
+function Home() {
+  return (
+    <div className="home">
+      <main>
+        <h1>FRONT-DEV</h1>
+        <p>ALL your frontend needs</p>
+      </main>
+      <div className="home2">
+        
+      </div>
+    </div>
+  );
+}
+
+export default Home;
